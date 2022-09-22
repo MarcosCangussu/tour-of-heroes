cd8b1b54cc94fe2de60b6488673f7c4d04fc62fd
Marcos Cangussu <marcos.cangussu@hotmail.com>
2022-09-22 19:02:41 -0300

    css condicional

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from 'src/app/hero.model';
+import { Heroes } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
-    name:'Magneto'
+    name: 'Magneto',
+  };
+  heroes = Heroes;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero.model';
+
+export const Heroes: Hero[] = [
+  {
+    id: 1,
+    name: 'Logan',
+  },
+  {
+    id: 2,
+    name: 'Hulk',
+  },
+  {
+    id: 3,
+    name: 'Ironman',
+  },
+  {
+    id: 3,
+    name: 'Jean Grey',
+  },
+  {
+    id: 5,
+    name: 'Thor',
+  },
+];
