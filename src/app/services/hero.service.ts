0a1e4bb7e966ffb564d89f0501aecd90148d119e
Marcos Cangussu <marcos.cangussu@hotmail.com>
2022-09-22 21:10:08 -0300

    obsevable

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from '../hero.model';
 import { Heroes } from '../mock-heroes';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  getHeroes(): Observable <Hero[]> {
+    const heroes = of(Heroes)
 
-  getHeroes(): Hero[] {
-    return Heroes;
+    return heroes;
   }
 }
+
+// Observable emite notificações sempre que ocorre uma mudança em um de seus itens e a partir disto podemos executar uma ação.
