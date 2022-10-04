739a4b98ac3739d2aeb5e178b103fc313f9597e6
Marcos Cangussu <marcos.cangussu@hotmail.com>
2022-10-03 21:58:28 -0300

    add token

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,6 +11,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 const Components = [
   MessagesComponent,
@@ -22,7 +23,7 @@ const Components = [
 const Modules = [FlexLayoutModule, MaterialModule, RouterModule];
 
 @NgModule({
-  declarations: [Components, ],
+  declarations: [Components ],
   imports: [CommonModule, Modules],
   exports: [Components, Modules],
   providers: [
@@ -34,6 +35,11 @@ const Modules = [FlexLayoutModule, MaterialModule, RouterModule];
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
       multi: true
     }
   ]
--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -4,29 +4,39 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { MessagesService } from '../services/messages.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
-
   constructor(private messagesService: MessagesService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
-      let errMsg = '';
-      if (err.error instanceof ErrorEvent) {
-        errMsg = `Error: ${err.error.message}`;
-      } else {
-        errMsg = `Error code: ${err.status}, message: ${err.message}`;
-      }
+        if (!environment.production) {
+          console.log(err);
+        }
+        let ErrorMsg = '';
+        if (err.error instanceof ErrorEvent) {
+          ErrorMsg = `Error: ${err.error.message}`;
+        } else if (Array.isArray(err.error) && err.error.length) {
+          ErrorMsg = `Error: ${err.error[0]}`;
+        } else if (err.error.errors) {
+          ErrorMsg = `Error: ${err.error.errors}`;
+        } else {
+          ErrorMsg = `Error code: ${err.status}, message: ${err.message}`;
+        }
 
-      this.messagesService.add(errMsg);
-      return throwError(() => new Error(errMsg));
-    })
-    )
+        this.messagesService.add(ErrorMsg);
+        return throwError(() => new Error(ErrorMsg));
+      })
+    );
   }
 }
